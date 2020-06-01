b527c944b5812b0d07b8510e9a4944b602264e8f
Ben Keyes <keyes.benjamin@gmail.com>
2020-06-01 11:20:13 -0700

    added new functions to make parsing text easier

--- example.py
+++ example.py
@@ -2,6 +2,7 @@
 import pdf2image
 from PIL import Image
 import pytesseract
+import re
 
 
 def pdf_to_img(pdf_file):
@@ -15,13 +16,40 @@ def ocr_core(file):
 
 def print_pages(pdf_file):
     images = pdf_to_img(pdf_file)
-    
     lst = []
     
     for pg, img in enumerate(images):
         lst.append(ocr_core(img))
-
+    
     return lst
 
-dst = print_pages('Test_Images/oteAnalyst.pdf')
+
+def removeBlankLines(lst):
+    lstSplit = lst.split('\n')
+    if isinstance(lstSplit, list):
+        no_blanks=[x for x in lstSplit if x not in  [' ', '']]
+        return '\n'.join(no_blanks)
+    else:
+        return [lstSplit]
+
+def returnDict(full_lst):
+    dct = {}
+    for i, item in enumerate(full_lst):
+        dct[i] = removeBlankLines(item)
+    
+    return dct
+
+
+dst = print_pages('Test_Images/oteAnalyst2.pdf')
+dct = returnDict(dst)
+
+
 print(dst)
+
+with open('mynewfile.txt', 'w+') as myfile:
+    for i,item in enumerate(dst):
+        if i == 0:
+            myfile.write(str("----Page num " + str(i)) + "----\n")
+        else:
+            myfile.write(str("\n----Page num " + str(i)) + "----\n")
+        myfile.write(removeBlankLines(item))
